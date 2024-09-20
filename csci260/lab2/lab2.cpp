2be1b3cb0876ab4e8d94d46c0f249f133bba3f6a
caleb <calebburke91@gmail.com>
2024-09-19 23:10:17 -0700

    lab2 impl

--- csci260/lab2/lab2.cpp
+++ csci260/lab2/lab2.cpp
@@ -1,28 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 #define P(x) std::cout << x << std::endl
 
 class LinkedList {
   public:
     // constructor
-    LinkedList(int capacity){
-      DataPile = new Cell[capacity];
+    LinkedList(int capacity) : capacity{capacity}, size{1} {
+      this->DataPile = new Cell[capacity];
+      
+      // create dummy node
+      this->DataPile[0].data = 0.0;
+      this->DataPile[0].next = 0;
+      this->headIndex = 0;
+
+      // set each cell next to -1 which means its an available spot
+      for(int i = 1; i < capacity; i++){
+        this->DataPile[i].next = -1;
+      } 
     }
 
     // ignore duplicates, returns false if list already full{}
     bool insert(double newData){
-      if(DataPile == 0){ 
-        Cell n{newData, 1};
-        DataPile[0] = n;
+      // check if available space
+      if(this->capacity < this->size + 1){
+        return false;
       }
-    } 
+
+      // find next available spot
+      for(int i = 0; i < this->capacity; i++){
+        if(this->DataPile[i].next == -1){
+          // i is index of new cell
+          this->DataPile[i].data = newData;
+          this->DataPile[i].next = 0;
+          
+          // set head index
+          this->DataPile[this->headIndex].next = i;
+          this->headIndex = i;
+          this->size++;
+
+          return true;
+        }
+      }
+
+      std::cout << "Unknown error in insert" << std::endl;
+      return false;
+    }
 
     // return the index of the array cell that contains parameter data
     // or return -1 if data doesn't exist in the list
     int find(double data){
       for(int i = 0; i < this->size; i++){
-        if(DataPile[i].data == data){
+        if(this->DataPile[i].data == data){
           return i;
         }
       }
@@ -32,19 +62,53 @@ class LinkedList {
     // return false if oldData doesn't exist in the list
     // or if the list already full
     bool insertAfter(double oldData, double newData){
-
+      (void)oldData;
+      (void)newData;
+      return false;
     }
 
     // return false if data doesn't exist in the list
     bool remove(double data){
+      // find index of data
+      for(int i = 0; i < this->size; i++){
+        int nextIndex = this->DataPile[i].next;
 
+        if(this->DataPile[nextIndex].data == data){
+          if(nextIndex == 0){
+            return false;
+          } else if(nextIndex == this->headIndex){
+            this->DataPile[i].next = 0;
+            this->DataPile[nextIndex].next = -1;
+            this->headIndex = i;
+          } else {
+            this->DataPile[i].next = this->DataPile[nextIndex].next;
+            this->DataPile[nextIndex].next = -1;
+          }
+          
+          this->size--;
+          return true;
+        }
+      }
+
+      return false;
     }
 
     // for testing purposes, display all data in the list
     void traverse(){
-      for(int i = 0; i < size; i++){
-        std::cout << DataPile[i].data << std::endl;
-      }
+      // std::cout << "HEAD " << this->headIndex << std::endl;
+      // for(int i = 0; i < this->capacity; i++){
+      //   std::cout << this->DataPile[i].data << " " << this->DataPile[i].next << std::endl;
+      // }
+
+      // skip dummy node
+      int i = this->DataPile[0].next;
+      
+      do {
+        std::cout << "Cell{ data: " << this->DataPile[i].data << ", next: " << this->DataPile[i].next << " } --> ";
+        i = this->DataPile[i].next;
+      } while(i != 0);
+
+      std::cout << "EMPTY" << std::endl;
     }
 
     // destructor
@@ -58,19 +122,44 @@ class LinkedList {
         int next;    // link part of the linked list node
     };
 
-    // suggested data fields
     int capacity;
-    int size; // optional
-    Cell *DataPile;
-    int freeListHead;
-    int listHead;
-    // any other data fields that could be useful
+    int size;
+    Cell* DataPile;
+    // int freeListHead;
+    int headIndex;
 };
-
 
 int main(int argc, const char** argv){
   (void)argc;
   (void)argv;
+
+  LinkedList list(10);
+
+  assert(list.insert(11.0));
+  assert(list.insert(-12.0));
+  assert(list.insert(0.0));
+  assert(list.insert(9871.142));
+  assert(list.insert(3.0));
+  assert(list.insert(2.0));
+  list.traverse();
+
+  P("Remove 0.0");
+  list.remove(0.0);
+  list.traverse();
+
+  P("Remove -12");
+  list.remove(-12.0);
+  list.traverse();
+
+
+  P("Remove first node");
+  list.remove(11.0);
+  list.traverse();
+
+
+  P("Remove 2");
+  list.remove(2.0);
+  list.traverse();
+
   return 0;
 }
-
